a4deae50af44f49c4a0010113d5e9b0a1d6b6883
tommchale <tom.mchale@icloud.com>
2022-05-02 10:11:58 +0100

    3 attempts and frame resize

--- RPS_template.py
+++ RPS_template.py
@@ -1,19 +1,21 @@
+from pickle import FRAME
 import random
 import cv2
 from keras.models import load_model
 import numpy as np
 import time
+import statistics
 
 
 class RPS:
     def __init__(self):
         self.rps_options = ['rock', 'paper', 'scissors']
+        self.computer_win_count = 0
+        self.user_win_count = 0
 
     def get_computer_choice(self):
 
         self.computer_choice = random.choice(self.rps_options)
-
-        return self.computer_choice
 
     def get_user_choice(self):
         # loads the tenable model
@@ -25,6 +27,9 @@ class RPS:
         # numpy.ndarray(shape, dtype=float, buffer=None, offset=0, strides=None, order=None)[source]
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
         timeout = time.time() + 5
+
+        predicition_list = []
+
         while time.time() < timeout:
 
             # Captures the video frame by frame
@@ -40,22 +45,28 @@ class RPS:
                                 127.0) - 1  # Normalize the image
             data[0] = normalized_image
             prediction = model.predict(data)
+
             if prediction[0][0] > 0.5:
+                predicition_list.append('rock')
                 print('Rock')
-                self.user_choice = "rock"
+
             elif prediction[0][1] > 0.7:
+                predicition_list.append('scissors')
                 print('Scissors')
-                self.user_choice = "scissors"
+
             elif prediction[0][2] > 0.5:
+                predicition_list.append('paper')
                 print("Paper")
-                self.user_choice = "paper"
+
             elif prediction[0][3] > 0.5:
+                predicition_list.append('nothing')
                 print("Nothing")
 
             # shows the image from the frame read by the camera
-            cv2.imshow('frame', frame)
+            imS = cv2.resize(frame, (480, 270))                # Resize image
+            cv2.imshow('frame', imS)
             # Press q to close the window
-            print(prediction)
+            # print(prediction)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -64,7 +75,7 @@ class RPS:
         # Destroy all the windows
         cv2.destroyAllWindows()
 
-        return self.user_choice
+        self.user_choice = statistics.mode(predicition_list)
 
     def get_winner(self):
 
@@ -72,6 +83,7 @@ class RPS:
         self.get_user_choice()
 
         if self.computer_choice == self.user_choice:
+            self.winner = 'nothing'
             print(' \n You chose', self.user_choice, ' \n')
             print(' \n The computer chose', self.computer_choice, ' \n')
             print('draw')
@@ -81,7 +93,6 @@ class RPS:
             print(' \n You chose', self.user_choice, ' \n')
             print(' \n The computer chose', self.computer_choice, ' \n')
             print('\n The winner is...', self.winner)
-            return self.winner
 
         else:
             self.winner = 'user'
@@ -89,12 +100,29 @@ class RPS:
             print(' \n The computer chose', self.computer_choice, ' \n')
             print('\n The winner is...', self.winner)
 
-            return self.winner
+    def play_game(self):
+        while True:
+            self.get_winner()
+            if self.winner == 'computer':
+                self.computer_win_count += 1
+                print('\n The score is Computer: ',
+                      self.computer_win_count, ' - User: ', self.user_win_count)
+            elif self.winner == 'user':
+                self.user_win_count += 1
+                print('\n The score is Computer: ',
+                      self.computer_win_count, ' - User: ', self.user_win_count)
+
+            if self.computer_win_count == 3:
+                print(' \n The computer has won ! \n')
+                break
+            elif self.user_win_count == 3:
+                print(' \n The user has wone ! \n')
+                break
 
 
 def play():
     game = RPS()
-    game.get_winner()
+    game.play_game()
 
 
 play()
