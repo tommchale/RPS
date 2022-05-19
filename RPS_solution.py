cecd6933d021cd56bdb15533b9b528bd5b618622
tommchale <tom.mchale@icloud.com>
2022-05-19 17:03:47 +0100

    further clean up

--- RPS_solution.py
+++ RPS_solution.py
@@ -18,49 +18,46 @@ class RPS:
         self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
         # Captures the video frame by frame
         # Define text details
-        self.text = "Press 'c' to continue..."
-        self.coordinates = (300, 300)
         self.font = cv2.FONT_HERSHEY_SIMPLEX
-        self.fontScale = 2
+        self.fontScale = 1
         self.color = (255, 0, 255)
         self.thickness = 2
+        self.message_center = "Press 'c' to continue..."
+        self.message_upper_center = ""
+        self.message_lower_center = ""
+        self.message_lower_right = ""
+        self.message_lower_left = ""
+        self.exit_flag = False
 
     def get_computer_choice(self):
 
         self.computer_choice = random.choice(self.rps_options[:3])
 
-    def open_camera_with_text(self):
+    def run_camera(self):
 
-        # opens the camera and defines a capture object
-        self.cap = cv2.VideoCapture(0)
-        ret, frame = self.cap.read()
+        ret, self.frame = self.cap.read()
 
-        frame = cv2.putText(frame, self.text, self.coordinates, self.font,
-                            self.fontScale, self.color, self.thickness, cv2.LINE_AA)
-
+        cv2.putText(self.frame, self.message_center, (100, 200), self.font,
+                    self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+        cv2.putText(self.frame, self.message_upper_center, (100, 100), self.font,
+                    self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+        cv2.putText(self.frame, self.message_lower_center, (100, 300), self.font,
+                    self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+        cv2.putText(self.frame, self.message_lower_left, (100, 400), self.font,
+                    self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+        cv2.putText(self.frame, self.message_lower_right, (100, 500), self.font,
+                    self.fontScale, self.color, self.thickness, cv2.LINE_AA)
         # Resize image
-        frame = cv2.resize(frame, (480, 270))
-        # shows the image from the frame read by the camera
-        cv2.imshow('frame', frame)
+        #self.frame = cv2.resize(self.frame, (480, 270))
 
-    def open_camera_for_prediction(self):
-        # opens the camera and defines a capture object
-        self.cap = cv2.VideoCapture(0)
-        ret, frame = self.cap.read()
-        # resize takes cv2.resize(src, dsize[, dst[, fx[, fy[, interpolation]]]])
-        # where src is the original image
-        # dsize is the desired size
-        # interpolation decides which pixel gets which value
         resized_frame = cv2.resize(
-            frame, (224, 224), interpolation=cv2.INTER_AREA)
+            self.frame, (224, 224), interpolation=cv2.INTER_AREA)
         image_np = np.array(resized_frame)
         normalized_image = (image_np.astype(np.float32) /
                             127.0) - 1  # Normalize the image
         self.data[0] = normalized_image
-        # Resize image
-        frame = cv2.resize(frame, (480, 270))
         # shows the image from the frame read by the camera
-        cv2.imshow('frame', frame)
+        cv2.imshow('Camera Open', self.frame)
 
     def close_and_destroy_windows(self):
         # After the loop release the cap object
@@ -68,104 +65,133 @@ class RPS:
         # Destroy all the windows
         cv2.destroyAllWindows()
 
-    def get_user_choice(self):
-
-        timeout = time.time() + 5
-
-        predicition_list = []
+    def intro(self):
 
         while True:
-
+            self.message_upper_center = ""
+            self.run_camera()
             # Define text details
-            self.text = "Press 'c' to continue..."
-            self.open_camera_with_text()
+            self.message_center = "Press 'q' to quit or 'c' to play RPS!"
             # Press c to continue the window
             if cv2.waitKey(1) & 0xFF == ord('c'):
                 break
-
-        timeout = time.time() + 3
-
-        while (timeout - time.time()) > 0:
-
-            self.text = (
-                f"{timeout - time.time():.0f}")
-            self.open_camera_with_text()
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                self.exit_flag = True
                 break
 
-        self.close_and_destroy_windows()
+    def present_choice(self):
+        timeout = time.time() + 4
+        while (timeout - time.time()) > 0:
+            self.run_camera()
+            self.message_upper_center = "Please present choice in..."
+            self.message_center = (
+                f"{timeout - time.time():.0f}")
 
-        timeout_2 = time.time() + 2
-
-        # self.close_and_destroy_windows()
-
-        while time.time() < timeout_2:
-
-            self.open_camera_for_prediction()
-            index = self.get_prediction()
-            predicition_list.append(index)
-
-        mode_index = statistics.mode(predicition_list)
-        self.user_choice = self.rps_options[mode_index]
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
     def get_prediction(self):
         prediction = self.model.predict(self.data)
         index = np.argmax(prediction[0])
         return index
 
+    def compute_prediction(self):
+
+        predicition_list = []
+        timeout = time.time() + 0.5
+        while (timeout - time.time()) > 0:
+
+            self.run_camera()
+            index = self.get_prediction()
+            predicition_list.append(index)
+
+        mode_index = statistics.mode(predicition_list)
+        self.user_choice = self.rps_options[mode_index]
+
+    def game_over(self):
+
+        # opens the camera and defines a capture object
+        self.cap = cv2.VideoCapture(0)
+
+        while True:
+            self.message_center = "Press 'p' to play again, or 'q' to quit."
+            self.message_lower_left = ""
+            self.message_lower_right = ""
+            self.message_upper_center = ""
+            self.run_camera()
+            if cv2.waitKey(0) & 0xFF == ord('q'):
+                self.exit_flag = True
+                break
+            if cv2.waitKey(0) & 0xFF == ord('p'):
+                self.computer_win_count = 0
+                self.user_win_count = 0
+                self.close_and_destroy_windows()
+                break
+
+    def get_user_choice(self):
+
+        # opens the camera and defines a capture object
+        self.cap = cv2.VideoCapture(0)
+        while True:
+
+            self.intro()
+            self.present_choice()
+            self.compute_prediction()
+            self.close_and_destroy_windows()
+            break
+
     def get_winner(self):
 
         self.get_computer_choice()
         self.get_user_choice()
 
+        self.message_lower_left = (
+            f" User: {self.user_choice} Computer: {self.computer_choice}")
+
         if self.user_choice == 'nothing':
             self.winner = 'none'
-            print(' \n You chose', self.user_choice, ' \n')
-            print(' \n The computer chose', self.computer_choice, ' \n')
 
         if self.computer_choice == self.user_choice:
             self.winner = 'nothing'
-            print(' \n You chose', self.user_choice, ' \n')
-            print(' \n The computer chose', self.computer_choice, ' \n')
-            print('draw')
+            self.message_lower_right = (
+                f"That was a draw. The score is Computer: {self.computer_win_count} - User: {self.user_win_count}")
 
         elif (self.computer_choice == 'paper' and self.user_choice == 'rock') or (self.computer_choice == 'scissors' and self.user_choice == 'paper') or (self.computer_choice == 'rock' and self.user_choice == 'scissors'):
             self.winner = 'computer'
-            print(' \n You chose', self.user_choice, ' \n')
-            print(' \n The computer chose', self.computer_choice, ' \n')
-            print('\n The winner is...', self.winner)
 
         elif self.user_choice != 'nothing':
             self.winner = 'user'
-            print(' \n You chose', self.user_choice, ' \n')
-            print(' \n The computer chose', self.computer_choice, ' \n')
-            print('\n The winner is...', self.winner)
 
     def play_game(self):
         # loads the tenable model
         self.model = load_model('keras_model.h5')
 
-        while True:
+        while self.exit_flag == False:
+
             self.get_winner()
+
             if self.winner == 'none':
                 print('Please go again and chose Rock, Paper or Scissors')
+                self.message_lower_right = (
+                    f"Please show an option and try again")
             if self.winner == 'computer':
                 self.computer_win_count += 1
-                print('\n The score is Computer: ',
-                      self.computer_win_count, ' - User: ', self.user_win_count)
+                self.message_lower_right = (
+                    f"The score is Computer: {self.computer_win_count} - User: {self.user_win_count}")
             elif self.winner == 'user':
                 self.user_win_count += 1
-                print('\n The score is Computer: ',
-                      self.computer_win_count, ' - User: ', self.user_win_count)
+                self.message_lower_right = (
+                    f"The score is Computer: {self.computer_win_count} - User: {self.user_win_count}")
 
             if self.computer_win_count == 1:
-                print(' \n The computer has won ! \n')
-                break
-            elif self.user_win_count == 1:
-                print(' \n The user has won ! \n')
-                break
+                self.message_lower_center = (
+                    f"Unlucky the computer won {self.computer_win_count} : {self.user_win_count} ")
+                self.game_over()
 
-            self.close_and_destroy_windows()
+            elif self.user_win_count == 1:
+                self.message_lower_center = (
+                    f"Congratulations you beat the computer {self.user_win_count} : {self.computer_win_count} ")
+                self.game_over()
 
 
 def play():
